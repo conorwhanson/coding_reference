3fa49278048e04ca26df915935518cab662c0ed3
Conor Hanson <conor@acetinc.com>
2025-03-10 15:58:45 -0500

    Testing JS 3.10

---
+++ JavaScript/TalkPythonTraining/hello.js
@@ -0,0 +1,17 @@
+// function hello(name) {
+//     return 'Hello, ' + name; 
+// }
+
+// console.log(hello("TESTING"));
+
+const minAge = 18;
+let myAge = 17;
+
+// if(myAge >= minAge){
+//     console.log("Congrats, you can drive");
+// } else {
+//     console.log("Sorry youngin. No skrt skrt for you.");
+// }
+
+// Shorter for loop
+(myAge >= minAge)? 'Congrats' : 'Sorry nerd';
