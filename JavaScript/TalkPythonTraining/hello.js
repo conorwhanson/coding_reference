dc6c66a7a06540ae83ab3ba975cfc5e9d9cfac80
Conor Hanson <conor@acetinc.com>
2025-03-11 11:59:38 -0500

    Testing JS 3..11

--- JavaScript/TalkPythonTraining/hello.js
+++ JavaScript/TalkPythonTraining/hello.js
@@ -4,8 +4,8 @@
 
 // console.log(hello("TESTING"));
 
-const minAge = 18;
-let myAge = 17;
+// const minAge = 18;
+// let myAge = 17;
 
 // if(myAge >= minAge){
 //     console.log("Congrats, you can drive");
@@ -14,4 +14,23 @@ let myAge = 17;
 // }
 
 // Shorter for loop
-(myAge >= minAge)? 'Congrats' : 'Sorry nerd';
+// (myAge >= minAge)? 'Congrats' : 'Sorry nerd';
+
+// FUNCTIONS
+// function hello(name) {
+//     console.log("Hello " + name);
+// }
+
+// hello("Chungus");
+const names = ['tim', 'sara', 'georg', 'dorcas'];
+const ages = [14, 12, 33, 9];
+const minAge = 18;
+
+for (let i = 0; i < names.length; i++){
+    let allowed = 'allowed'
+    if (ages[i] < minAge){
+        allowed = 'not ' + allowed;
+    }
+
+    console.log(`${names[i]} (${ages[i]}) is ${allowed} to drive`);
+}
